1ae037681eb633703ed6c7e34043af484bae802b
wstan2 <walter.scott.tan@gmail.com>
2019-08-12 14:34:34 -0500

    fixed grid and added color and resize buttons

--- main.js
+++ main.js
@@ -1,27 +1,71 @@
-drawGrid(16);
-let paintColor = "gray";
+let paintColor = "black";
 
-function drawGrid(size) {
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      let gridCell = document.createElement("div");
-      gridCell.style.height = "25px";
-      gridCell.style.width = "25px";
-      gridCell.style.background = "white";
-      gridCell.classList.add("finalGrid");
-      mainGrid.appendChild(gridCell);
-    }
+const mainGrid = document.querySelector(".mainGrid");
 
-    let newLine = document.createElement("br");
-    mainGrid.appendChild(newLine);
+let drawGrid = size => {
+  let resizable = 500 / size + "px";
+
+  for (let i = 0; i < size * size; i++) {
+    let gridCell = document.createElement("div");
+    gridCell.style.height = resizable;
+    gridCell.style.width = resizable;
+    gridCell.style.background = "white";
+    gridCell.classList.add("finalGrid");
+    mainGrid.appendChild(gridCell);
   }
-}
+};
+
+let reset = () => {
+  let gridCells = document.querySelectorAll(".finalGrid");
+  gridCells.forEach(gridCell => {
+    gridCell.style.background = "white";
+  });
+};
+
+let erase = () => {
+  paintColor = "white";
+};
+
+let black = () => {
+  paintColor = "black";
+};
+
+let random = () => {
+  let hue =
+    "rgb(" +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    ")";
+
+  paintColor = hue;
+};
+
+let resizeGrid = () => {
+  let gridCells = document.querySelectorAll(".finalGrid");
+  var br = document.querySelectorAll("br");
+
+  br.forEach(br => br.remove());
+
+  gridCells.forEach(gridCell => gridCell.remove());
+
+  let newSize = prompt("Please input a new size");
+
+  drawGrid(newSize);
+};
 
 let paint = e => {
   const target = e.target;
-  target.style.backgroundColor = paintColor;
+
+  if (paintColor != "black" && paintColor != "white") {
+    random();
+    target.style.backgroundColor = paintColor;
+  } else {
+    target.style.backgroundColor = paintColor;
+  }
 };
+drawGrid(16);
 
 mainGrid.addEventListener("mouseover", paint);
-
-
